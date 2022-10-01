eaa7db15093607cea9d1e73e3188724f06dce647
CheemsBread505 <CheemsBread505@proton.me>
2022-09-30 23:56:36 -0400

    Added the .exe and main.cpp file

---
+++ README.md
@@ -0,0 +1,2 @@
+# executable opens website
+ A executable file made in C++ that opens a website
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+int main() {
+    // replace my url with the one you want to use
+   ShellExecute(NULL, "open", "https://www.cheemsbreaddoescode.cf/",
+   NULL, NULL, SW_SHOWNORMAL);
+
+  return 0;
+}
